8cecad993c5a539b2cdd3d8a3c986c732f2e0cef
Charles Eckman <charles@cceckman.com>
2017-12-22 20:40:47 -0800

    fix path of dep

--- debbie.sh
+++ debbie.sh
@@ -367,7 +367,7 @@ fi
 # Go get go tools
 go get -u github.com/derekparker/delve/cmd/dlv
 go get -u github.com/github/hub
-go get -u github.com/golang/dep
+go get -u github.com/golang/dep/cmd/dep
 
 # Manually install Helm, since there aren't repositoried packages.
 HELM_VERSION="2.7.2"
