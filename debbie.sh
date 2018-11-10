ee598302017e227b754f7d734d8dbf7a553a5557
Charles Eckman <cceckman@google.com>
2018-11-09 20:22:15 -0800

    Syntax / mkdir fixes

--- debbie.sh
+++ debbie.sh
@@ -146,7 +146,7 @@ git config --global merge.tool vimdiff
 git config --global difftool.prompt false
 
 # Set Github public key.
-mkdir ~/.ssh
+mkdir -p ~/.ssh
 echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="  >> $HOME/.ssh/known_hosts
 
 
@@ -176,7 +176,7 @@ then
     && git reset --hard \
     && git submodule update --recursive --init \
     && git remote set origin git@github.com:cceckman/Tilde.git \
-    && git remote set --push origin git@github.com:cceckman/Tilde.git \
+    && git remote set --push origin git@github.com:cceckman/Tilde.git
   } || {
     x=$?
     echo "Failed to load Tilde into \$HOME!"
