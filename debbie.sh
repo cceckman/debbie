3ec6db61e24e5be1eedb1c520519510a310c930e
Charles Eckman <cceckman@google.com>
2019-01-18 15:34:21 -0800

    install go LSP servers

--- debbie.sh
+++ debbie.sh
@@ -371,6 +371,7 @@ go get -u github.com/derekparker/delve/cmd/dlv
 go get -u github.com/github/hub
 go get -u github.com/golang/dep/cmd/dep
 go get -u github.com/bazelbuild/buildtools/buildifier
+go get -u golang.org/x/tools/cmd/gopls
 
 # Manually install Helm, since there aren't repositoried packages.
 if ! which helm || ! vergte "$HELM_VERSION" "$(helm version -c --short)"
