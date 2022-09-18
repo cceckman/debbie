b8b0579f5cf7758695887f71d5a295c70d6d6f8d
Charles Eckman <charles@cceckman.com>
2022-09-18 06:08:54 -0700

    update key reception for github CLI

--- debbie.sh
+++ debbie.sh
@@ -278,8 +278,10 @@ SOURCES
   curl -Lo- https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 
   # Use Github's servers for the `gh` CLI
-  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-  sudo apt-add-repository https://cli.github.com/packages
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+    | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 
   sudo apt-get update
 }
